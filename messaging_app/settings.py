cb0504d386d39bf65a9830cd6e7aaa975d0d600b
Samuel Mabuka <samuelmabuka@gmail.com>
2025-07-18 19:09:14 +0300

    Project set up

--- messaging_app/settings.py
+++ messaging_app/settings.py
@@ -14,10 +14,10 @@ INSTALLED_APPS = [
 
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated'
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication'
     ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated'
-    ]
 }
