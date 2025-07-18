58bf884e3091b5b7e2a2b91d95c90175641ce6ce
Samuel Mabuka <samuelmabuka@gmail.com>
2025-07-18 18:53:05 +0300

    Project set up

--- messaging_app/settings.py
+++ messaging_app/settings.py
@@ -1,11 +1,5 @@
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
 
+INSTALLED_APPS = [
     'rest_framework',
 
     'chats',            
