6836b325480a3b4c1e7a8932240a5069b1b3b797
Samuel Mabuka <samuelmabuka@gmail.com>
2025-07-20 14:53:29 +0300

    Build api endpoints with views

--- messaging_app/urls.py
+++ messaging_app/urls.py
@@ -5,3 +5,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('chats.urls')), 
 ]
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('chats.urls')),  # ✅ This must include "api/"
+]
