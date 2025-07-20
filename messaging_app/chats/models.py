a3453c26b3ffcc6ab0633dabd023db5d0265ffb8
Samuel Mabuka <samuelmabuka@gmail.com>
2025-07-20 14:28:49 +0300

    Define data Models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -2,22 +2,26 @@ import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class User(AbstractUser):
-    ROLE_CHOICES = (
-        ('guest', 'Guest'),
-        ('host', 'Host'),
-        ('admin', 'Admin'),
-    )
-
+class User(AbstractBaseUser, PermissionsMixin):
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=150, null=False)
+    last_name = models.CharField(max_length=150, null=False)
+    email = models.EmailField(unique=True, null=False)
+    
+    password = models.CharField(max_length=128, null=False)
+
+    password_hash = models.CharField(max_length=255, null=False)
     phone_number = models.CharField(max_length=20, null=True, blank=True)
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='guest')
+    role = models.CharField(
+        max_length=10,
+        choices=[('guest', 'Guest'), ('host', 'Host'), ('admin', 'Admin')],
+        null=False
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
-    username = None  # We’ll use email for login instead of username
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
+
 
     def __str__(self):
         return f"{self.email} ({self.role})"
