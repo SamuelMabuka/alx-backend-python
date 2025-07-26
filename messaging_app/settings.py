9a4f5f4758c19d96ccc44e3b30d5016a17bd77d6
Samuel Mabuka <samuelmabuka@gmail.com>
2025-07-27 01:46:51 +0300

    Pagination and Filtering

--- messaging_app/settings.py
+++ messaging_app/settings.py
@@ -1,10 +1,8 @@
 from pathlib import Path
 from datetime import timedelta
 
-# Base directory
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -12,18 +10,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    # Third-party
     'rest_framework',
     'rest_framework_simplejwt',
     'oauth2_provider',
-
-    # Local apps
     'chats',
 
 ]
 
-# Django REST Framework configuration
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
@@ -33,22 +26,17 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'chats.pagination.MessagePagination',  # ✅ use custom pagination
-    'PAGE_SIZE': 20,  # ✅ sets default page size
+    'DEFAULT_PAGINATION_CLASS': 'chats.pagination.MessagePagination',
+    'PAGE_SIZE': 20,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 
 
-
-# Use custom user model
 AUTH_USER_MODEL = 'chats.User'
 
-# JWT settings for SimpleJWT
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
 }
-
-# Other default D
