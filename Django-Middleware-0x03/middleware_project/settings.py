f53326a5b7cc08dfcbe8b9ab9bdb1367e4fd69a2
Samuel Mabuka <samuelmabuka@gmail.com>
2025-07-27 22:28:52 +0300

    Detect and Block offensive Language

--- Django-Middleware-0x03/middleware_project/settings.py
+++ Django-Middleware-0x03/middleware_project/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'chats.middleware.RestrictAccessByTimeMiddleware', 
+     'chats.middleware.OffensiveLanguageMiddleware',
 ]
 
 ROOT_URLCONF = 'messaging_app.urls'
